98a3042dcf512cd67dbf449023d8fdef61382444
Haritha Manikonda <haritha.manikonda@gmail.com>
2021-08-07 10:16:30 -0700

    Updating frame handling in selenium

--- src/test/java/Selenium/SwitchToFrame.java
+++ src/test/java/Selenium/SwitchToFrame.java
@@ -4,34 +4,38 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
-
-public class SwitchToFrame {
+import java.util.Iterator;
+import java.util.Set;
+public class SwitchToFrame 
+{
 	@Test
 	public void SwitchTo_Frame()
-	
+
 	{
 		System.setProperty("webdriver.chrome.driver", "/Users/arunakirannulu/eclipse-workspace/Selenium/chromedriver");
 		WebDriver driver=new ChromeDriver();
 		driver.get("http://demo.guru99.com/test/guru99home/"); 
-	       // navigates to the page consisting an iframe
 		
+		// navigates to the page consisting an iframe
+
 		driver.manage().window().maximize();
-	    /*   driver.switchTo().frame("a077aa5e"); //switching the frame by ID
+		
+		// Different way to switch to frame
+		/*
+		   - By Index    -    driver.switchTo().frame(1);
+           - By Name or Id -  driver.switchTo().frame("a077aa5e");
+                              driver.switchTo().frame("a077aa5e");
+           - By Web Element - driver.switchTo().frame(driver.findElement(By.name("a077aa5e")))
+		 */
+		
+		//switching the frame by ID
+		
+		driver.switchTo().frame("a077aa5e");; 
+		
+		// click on a link in iFrame
 
-			System.out.println("********We are switch to the iframe*******");
   		driver.findElement(By.xpath("html/body/a/img")).click();
-		    //Clicks the iframe
-    
-			System.out.println("*********We are done***************");*/
-		
-		
-		
-	    driver.switchTo().frame(1); //Switching to the frame
-		System.out.println("********We are switched to the iframe*******");
-		driver.findElement(By.xpath("html/body/a/img")).click();
-		
-		//Clicking the element in line with Advertisement
-	    System.out.println("*********We are done***************");
-	}
+  
 
+	}
 }
